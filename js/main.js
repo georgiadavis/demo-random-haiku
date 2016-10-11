2740a30820b22968f79db4763bed3fecb1e4eb11
Richard Kalehoff <richardkalehoff@gmail.com>
2016-10-11 11:47:27 -0700

    add 3 hiakus

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 (function () {
     var responseContainer = document.querySelector('#response-container');
+    var haikus = [
+        'I am first with five\nThen seven in the middle --\nFive again to end.',
+        'Green and speckled legs,\nHop on logs and lily pads\nSplash in cool water.',
+        'In a pouch I grow,\nOn a southern continent --\nStrange creatures I know.'
+    ];
 
     responseContainer.innerHTML = '';
 })();
